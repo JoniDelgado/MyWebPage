bef010892eb68c4afff60000a769ab69b25bf364
Jonatan Delgado <delgadojonatan.b@gmail.com>
2022-12-05 20:01:11 +0100

    agregado foto de perfil y estilos botones

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { StyleHeroSection } from "./Styled.hero";
+import profileImage from "../../assets/Images/imagen-perfil.jpeg";
 
 const Hero = () => {
   return (
     <StyleHeroSection>
-      <p>Hy there, I am</p>
-      <h1>Jontan Delgado</h1>
-      <h2>I'm a fron-end developer.</h2>
+      <div>
+        <img src={profileImage} alt="imagen de perfil" />
+      </div>
+      <div>
+        <p>Hy there, I am</p>
+        <h1>Jontan Delgado</h1>
+        <h2>I'm a fron-end developer.</h2>
+      </div>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo provident
         dolores ex dolorem, dicta tenetur placeat officia magnam mollitia neque
--- src/components/hero/Styled.hero.jsx
+++ src/components/hero/Styled.hero.jsx
@@ -2,5 +2,41 @@ import styled from "styled-components";
 
 export const StyleHeroSection = styled.section`
   margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
   background-color: #3f3b6c;
+
+  & > div:first-of-type {
+    margin: auto;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  button {
+    margin: 2rem auto;
+    padding: 0.7rem;
+    width: 50%;
+    background-color: #f0c9a3ce;
+    border: none;
+    border-radius: 5px;
+    color: #00005c;
+    font-size: 1rem;
+    font-weight: 600;
+    text-transform: uppercase;
+
+    &:hover {
+      background-color: #3b185f;
+      color: #d8d8d8;
+      box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px,
+        rgba(0, 0, 0, 0.22) 0px 15px 12px;
+    }
+  }
 `;
