a4155c9b564e86c1e7782caf2394ddd8270f3d7f
Jonatan Delgado <delgadojonatan.b@gmail.com>
2022-12-30 18:55:05 +0100

    configuracion formulario en index html

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,7 +6,8 @@ const Contact = () => {
     <StyleContactSection>
       <h2>Contacto</h2>
       <p>Puedes comunicarte conmigo desde el formulario de contacto:</p>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST">
+        <input type="hidden" name="form-name" value="contact" />
         <input type="text" name="name" placeholder="Nombre" />
         <input type="mail" name="email" placeholder="Email" />
         <textarea
